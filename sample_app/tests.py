973cd6ee985ffab52bb5bb4638e857ce41a1387a
banghybrid <bangna@hybrid-technologies.com.vn>
2023-05-05 14:37:33 +0700

    add model page

--- sample_app/tests.py
+++ sample_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from sample_app.models import Page
+
+
+class WebsiteTests(TestCase):
+    def test_page_is_created_successfully(self):
+        page = Page(
+            name='Home',
+            slug='home'
+        )
+        page.save()
