4bb9c90b0b325faf4d2002eea6375992249138e2
banghybrid <bangna@hybrid-technologies.com.vn>
2023-05-05 13:27:31 +0700

    init first commit

--- demo/manage.py
+++ demo/manage.py
@@ -2,11 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_k8s.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,5 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    dotenv.read_dotenv()
     main()
